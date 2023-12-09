1ddaa8b436ae582ee826d67f7dcecda54397cb87
Tsaplin Sergey <74894518+talatari@users.noreply.github.com>
2023-12-09 13:36:52 +0300

    Add class Animator and AnimatorParameters.

---
+++ Assets/Players/Scripts/AnimatorParameters.cs
@@ -0,0 +1,8 @@
+namespace Players
+{
+    public static class AnimatorParameters
+    {
+        public static readonly int Attack = UnityEngine.Animator.StringToHash(nameof(Attack));
+        public static readonly int Move = UnityEngine.Animator.StringToHash(nameof(Move));
+    }
+}
